aa3f2f1433a35fc13032dec93b0c4e22767a4be5
Alexandra Volkova <alexandr.sazhev@yandex.ru>
2022-01-04 14:56:41 +0500

    Bad macro task done

---
+++ red-belt/week1/bad_macro.cpp
@@ -0,0 +1,34 @@
+//#include <iostream>
+//
+//using namespace std;
+//
+//int main() {
+//	cout << "Hello, world!" << endl;
+//	return 0;
+//}
+
+#include "test_runner.h"
+
+#include <ostream>
+
+using namespace std;
+
+#define PRINT_VALUES(out, x, y) (out) << (x) << endl << (y) << endl
+
+int main() {
+	TestRunner tr;
+	tr.RunTest([] {
+		ostringstream output;
+		PRINT_VALUES(output, 5, "red belt");
+		ASSERT_EQUAL(output.str(), "5\nred belt\n");
+	}, "PRINT_VALUES usage example");
+	tr.RunTest([] {
+		ostringstream output;
+		int a, b;
+		cin >> a >> b;
+		if (a < b) PRINT_VALUES(output, 15, "red belt");
+		else PRINT_VALUES(output, 5, "red belt");
+		ASSERT_EQUAL(output.str(), "5\nred belt\n");
+	}, "PRINT_VALUES in else");
+	return 0;
+}
