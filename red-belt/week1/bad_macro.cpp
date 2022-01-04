f12492af68a1af1bfcd2f6549bce827b792480db
Alexandra Volkova <alexandr.sazhev@yandex.ru>
2022-01-04 14:58:43 +0500

    Bad macro task done

--- red-belt/week1/bad_macro.cpp
+++ red-belt/week1/bad_macro.cpp
@@ -15,7 +15,7 @@ using namespace std;
 
 #define PRINT_VALUES(out, x, y) (out) << (x) << endl << (y) << endl
 
-int main() {
+int main4() {
 	TestRunner tr;
 	tr.RunTest([] {
 		ostringstream output;
