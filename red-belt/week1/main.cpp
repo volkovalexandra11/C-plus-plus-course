34f6cf62df3acf79d2771ba69d564b72804c825d
Alexandra Volkova <alexandr.sazhev@yandex.ru>
2022-01-07 15:29:51 +0500

    Default main

--- red-belt/week1/main.cpp
+++ red-belt/week1/main.cpp
@@ -1,65 +1,43 @@
-//#include <iostream>
-//
-//using namespace std;
-//
-//int main() {
-//	cout << "Hello, world!" << endl;
-//	return 0;
-//}
-
 #include "test_runner.h"
+#include "deque.cpp"
+
+#include <iostream>
 
 using namespace std;
 
-template<typename T>
-class Table {
-public:
-	Table(size_t rows, size_t cols) {
-		Resize(rows, cols);
-	}
+void TestDeque() {
+	Deque<int> d;
+	ASSERT_EQUAL(d.Empty(), true)
 
-	pair<size_t, size_t> Size() const {
-		if (!content.empty() && !content[0].empty())
-			return {x, y};
-		return {0, 0};
-	}
+	d.PushBack(1);
+	ASSERT_EQUAL(d.Front(), 1);
+	ASSERT_EQUAL(d.Back(), 1);
 
-	void Resize(size_t rows, size_t cols) {
-		x = rows;
-		y = cols;
-		content.resize(rows);
-		for (auto& row: content) {
-			row.resize(cols);
-		}
-	}
+	d.PushBack(2);
+	ASSERT_EQUAL(d.Front(), 1);
+	ASSERT_EQUAL(d.Back(), 2);
 
-	vector<T>& operator[](size_t ind) {
-		return content[ind];
-	}
+	d.PushFront(3);
 
-	vector<T>& operator[](size_t ind) const {
-		return content[ind];
-	}
-
-private:
-	vector<vector<T>>content;
-	size_t x;
-	size_t y;
-};
-
-void TestTable() {
-	Table<int> t(1, 1);
-	ASSERT_EQUAL(t.Size().first, 1u);
-	ASSERT_EQUAL(t.Size().second, 1u);
-	t[0][0] = 42;
-	ASSERT_EQUAL(t[0][0], 42);
-	t.Resize(3, 4);
-	ASSERT_EQUAL(t.Size().first, 3u);
-	ASSERT_EQUAL(t.Size().second, 4u);
+	ASSERT_EQUAL(d.Front(), 3);
+	ASSERT_EQUAL(d.Back(), 2);
+	ASSERT_EQUAL(d.Size(), 3);
 }
 
 int main() {
+	cout << "Hello, world!" << endl;
 	TestRunner tr;
-	RUN_TEST(tr, TestTable);
+	RUN_TEST(tr, TestDeque);
 	return 0;
+
+	Deque<int> my_deque{};
+	my_deque.PushFront(1);
+	my_deque.PushFront(2);
+	my_deque.PushBack(1);
+	my_deque.PushFront(4);
+	my_deque.PushBack(2);
+	my_deque.PushBack(3);
+	for (size_t i = 0; i < my_deque.Size(); ++i) {
+		printf("%d ", my_deque.At(i)) ;
+	}
 }
