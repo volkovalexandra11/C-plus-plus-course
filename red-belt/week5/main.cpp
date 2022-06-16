3b2a8876a0107695ed39c80ac3a7faa153adae36
Alexandra Volkova <alexandr.sazhev@yandex.ru>
2022-06-16 23:44:08 +0500

    Task 6 of brown belt

--- red-belt/week5/main.cpp
+++ red-belt/week5/main.cpp
@@ -1,8 +1,6 @@
-#include <algorithm>
-#include <iostream>
 
 
-int main() {
-	std::cout << "Hello, World!" << std::endl;
-	return 0;
-}
+//int main() {
+//	std::cout << "Hello, World!" << std::endl;
+//	return 0;
+//}
